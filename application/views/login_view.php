e2ee517717f414e166a6199e51b94eef8120488e
mohsincox <mohsincse2015@gmail.com>
2017-08-19 10:57:23 +0600

    bs_another_form

--- application/views/login_view.php
+++ application/views/login_view.php
@@ -33,14 +33,16 @@
                                     <form id="login-form" action="<?php echo base_url();?>index.php/verifylogin" method="post" role="form" style="display: block;">
                                         <?php //echo form_open('verifylogin'); ?>
                                         <div class="form-group">
-                                            <input type="text" name="username" id="username" tabindex="1" class="form-control" placeholder="Username" value="">
+                                            <div class="input-group">
+                                                <span class="input-group-addon" ><i style="color: #53A3CD;" class="glyphicon glyphicon-user"></i></span>
+                                                <input type="text" name="username" id="username" tabindex="1" class="form-control" placeholder="Username" value="">
+                                            </div>
                                         </div>
-                                        <!-- <div class="input-group input-group-lg">
-                                            <span class="input-group-addon" ><i class="glyphicon glyphicon-user"></i></span>
-                                            <input type="text" class="form-control" placeholder="Username">
-                                        </div> -->
                                         <div class="form-group">
-                                            <input type="password" name="password" id="password" tabindex="2" class="form-control" placeholder="Password">
+                                            <div class="input-group">
+                                                <span class="input-group-addon" ><i style="color: #53A3CD;" class="glyphicon glyphicon-lock"></i></span>
+                                                <input type="password" name="password" id="password" tabindex="2" class="form-control" placeholder="Password">
+                                            </div>
                                         </div>
                                         <div class="form-group text-center">
                                             <input type="checkbox" tabindex="3" class="" name="remember" id="remember">
